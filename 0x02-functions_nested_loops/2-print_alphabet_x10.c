fa8a1f29ac9cbf4f45c1c0803cfeeaa3b4bee850
Francis-xavi <ahunna45@gmail.com>
2022-06-08 10:13:51 -0700

    task3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
+int main(void)
 {
+	 print_alphabet_x10();
+
 	int n, co;
 
 	co = 0;
@@ -19,4 +21,6 @@ void print_alphabet_x10(void)
 		co++;
 		_putchar('\n');
 	}
+
+	 return (0);
 }
